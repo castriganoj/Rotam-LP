d6ad83982ee6df2153596bc1af7f898fec17ed71
castriganoj <castriganoj@gmail.com>
2018-09-12 16:52:20 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rotam_LP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
