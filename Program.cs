8a210537a6ffe32fd0ba94916fc83819d80597ec
castriganoj <castriganoj@gmail.com>
2018-11-07 10:22:58 -0500

    Implement application insights thry the web host builder.

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace Rotam_LP
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
